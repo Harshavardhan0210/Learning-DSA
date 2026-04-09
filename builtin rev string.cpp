914475d7407370edf071957bdfd6ae89fbc02327
Harshavardhan M M <mmharshavardhan5@gmail.com>
2026-04-09 22:09:02 +0530

    Add files via upload

---
+++ builtin rev string.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std; 
+
+int main(){
+    string str;
+
+    cout<<"Enter the string :";
+    cin>>str;
+
+    string rev = str;
+
+    reverse(rev.begin(),rev.end());
+
+    cout<<"Reversed string :"<<rev;
+
+    return 0;
+}
